0832a81ed4a6f978464ab0d5a324fa8a756cf3cd
Eduardo Correa <correaherreraeduardo@gmail.com>
2023-04-08 19:44:40 -0500

    feat: menu

--- src/AnalysisDataTwo.py
+++ src/AnalysisDataTwo.py
@@ -3,6 +3,26 @@ import AnalysisDataTwoGraphs as graphics
 
 global data
 
+def getOption():
+    message = "Options \n"
+    message += "1 - Relationship between men and women by social class \n"
+    message += "2 - Passengers age histogram \n"
+    message += "3 - Survival rate by age \n"
+    message += "4 - Survivors and non-survivors by social class"
+    print('Task 3 - practical component - simulated practices')
+    print(message)
+    option = input('Enter the option of the graph you want to see: ')
+    return validateOption(option)
+
+def validateOption(option):
+    try:
+        if not option.isnumeric():
+            raise ValueError('Only numbers are allowed')
+        return int(option)
+    except ValueError as ve:
+        print(ve)
+        return False
+
 def getData():
     global data
     data = pd.read_csv('files/titanic.csv')
@@ -14,15 +34,23 @@ def prepareData():
     data['Cabin'] = data['Cabin'].fillna('NS')
     data['Embarked'] = data['Embarked'].fillna('N')
 
-def createGraphics():
+def createGraphics(option):
     global data
-    graphics.graphFour(data)
+    if option == 1:
+        graphics.graphOne(data)
+    elif option == 2:
+        graphics.graphTwo(data)
+    elif option == 3:
+        graphics.graphThree(data)
+    elif option == 4:
+        graphics.graphFour(data)
 
 def run():
-    print('Task 3 - practical component - simulated practices')
-    getData()
-    prepareData()
-    createGraphics()
+    option = getOption()
+    if option:
+        getData()
+        prepareData()
+        createGraphics(option)
 
 if __name__ == '__main__':
     run()
